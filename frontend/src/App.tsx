ae73925e0444b8a3bc40c00b61dbab6806f5e1c5
Bernat Duran <bduran@ohpen.com>
2021-11-11 18:08:19 +0100

    bootstrap react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Hello = styled.h1`
+  color: red;
+`;
+function App() {
+  return (
+    <div className="App">
+      <Hello>Hello World</Hello>
+    </div>
+  );
+}
+
+export default App;
