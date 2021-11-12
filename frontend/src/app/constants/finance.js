4b8e354947c910151cb23c537ba632d801d14cac
Bernat Duran <bduran@ohpen.com>
2021-11-12 23:13:01 +0100

    fix finance

---
+++ frontend/src/app/constants/finance.js
@@ -0,0 +1,115 @@
+export const initialBalance = {
+  balance: 0,
+  variationFromPreviousMonth: 0,
+  variationFromPreviousDay: 0,
+};
+
+export const initialQarters = {
+  q1: {
+    total: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    hr: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    realState: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    services: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+  },
+  q2: {
+    total: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    hr: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    realState: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    services: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+  },
+  q3: {
+    total: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    hr: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    realState: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    services: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+  },
+  q4: {
+    total: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    hr: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    realState: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+    services: {
+      expectedIncome: 0,
+      currentIncome: 0,
+      expectedOutcome: 0,
+      currentOutcome: 0,
+    },
+  },
+};
+
+export const quarters = ['q1', 'q2', 'q3', 'q4'];
+export const sections = ['hr', 'realState', 'services'];
--- frontend/src/app/pages/finance/Home.js
+++ frontend/src/app/pages/finance/Home.js
@@ -1,123 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getBalance, getQuarters } from '../../services/API';
+import { initialBalance, initialQarters, quarters, sections } from '../../constants/finance';
 
 import { formatNumber } from '../../helpers/formatNumber';
-
-const initialBalance = {
-  balance: 0,
-  variationFromPreviousMonth: 0,
-  variationFromPreviousDay: 0,
-};
-
-const initialQarters = {
-  q1: {
-    total: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    hr: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    realState: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    services: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-  },
-  q2: {
-    total: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    hr: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    realState: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    services: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-  },
-  q3: {
-    total: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    hr: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    realState: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    services: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-  },
-  q4: {
-    total: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    hr: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    realState: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-    services: {
-      expectedIncome: 0,
-      currentIncome: 0,
-      expectedOutcome: 0,
-      currentOutcome: 0,
-    },
-  },
-};
-
-const quarters = ['q1', 'q2', 'q3', 'q4'];
-const sections = ['hr', 'realState', 'services'];
 
 const Home = () => {
   const [data, setData] = useState(initialBalance);
@@ -131,6 +16,7 @@ const Home = () => {
   return (
     <div>
       <div className="page-header">
+        {console.log(quarterData)}
         <h3 className="page-title">Finance</h3>
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
