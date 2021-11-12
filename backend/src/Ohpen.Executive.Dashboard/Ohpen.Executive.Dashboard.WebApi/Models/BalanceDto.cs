072d5312ae98024cb98fe9dd4509962c62b9484d
Vinicius Scheidegger <vscheidegger@ohpen.com>
2021-11-12 17:47:59 +0100

    add balance endpoint

---
+++ backend/src/Ohpen.Executive.Dashboard/Ohpen.Executive.Dashboard.WebApi/Models/BalanceDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ohpen.Executive.Dashboard.WebApi.Models
+{
+    public class BalanceDto
+    {
+        public decimal Balance { get; set; }
+        public DateTime BalanceDateTime { get; set; }
+    }
+}
