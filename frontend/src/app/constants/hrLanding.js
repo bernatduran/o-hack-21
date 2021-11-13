9c34b20fefd2702ae12ae1bf474467af8ae0fa5a
Bernat Duran <bduran@ohpen.com>
2021-11-13 10:40:37 +0100

    feat HR page

---
+++ frontend/src/app/constants/hrLanding.js
@@ -0,0 +1,138 @@
+export const hrData = {
+  hiring: [
+    {
+      id: 1,
+      name: 'Han Solo',
+      role: 'Dev',
+      hiringStatus: {
+        culturalFit: true,
+        techAssignment: true,
+        peerReview: true,
+        offerMade: false,
+        screenOk: false,
+        hired: false,
+      },
+    },
+    {
+      id: 2,
+      name: 'Hans Gruber',
+      role: 'QA',
+      hiringStatus: {
+        culturalFit: true,
+        techAssignment: false,
+        peerReview: false,
+        offerMade: false,
+        screenOk: false,
+        hired: false,
+      },
+    },
+    {
+      id: 3,
+      name: 'Peter Venkman',
+      role: 'Sales',
+      hiringStatus: {
+        culturalFit: true,
+        techAssignment: false,
+        peerReview: true,
+        offerMade: true,
+        screenOk: false,
+        hired: false,
+      },
+    },
+    {
+      id: 4,
+      name: 'Mia Wallace',
+      role: 'Arch',
+      hiringStatus: {
+        culturalFit: false,
+        techAssignment: false,
+        peerReview: false,
+        offerMade: false,
+        screenOk: false,
+        hired: false,
+      },
+    },
+  ],
+  newHires: [
+    {
+      id: 1,
+      description: 'Elen Ripley',
+    },
+    {
+      id: 2,
+      description: 'Michael Corleone',
+    },
+  ],
+  leavers: [
+    {
+      id: 1,
+      description: 'John McClane',
+    },
+    {
+      id: 2,
+      description: 'Marty McFly',
+    },
+  ],
+  upcomingBirthdays: [
+    {
+      id: 1,
+      name: 'Tony Montana',
+      date: '2024/12/05',
+    },
+    {
+      id: 2,
+      name: 'Donnie Darko',
+      date: '2024/12/07',
+    },
+    {
+      id: 3,
+      name: 'Frank Drebin',
+      date: '2024/12/10',
+    },
+    {
+      id: 4,
+      name: 'Private William Hudson',
+      date: '2024/12/12',
+    },
+    {
+      id: 5,
+      name: 'Martin Blank',
+      date: '2024/12/17',
+    },
+    {
+      id: 6,
+      name: 'Ethan Hunt',
+      date: '2024/12/17',
+    },
+    {
+      id: 7,
+      name: 'Groot',
+      date: '2024/12/20',
+    },
+    {
+      id: 8,
+      name: 'Hal',
+      date: '2024/12/21',
+    },
+    {
+      id: 9,
+      name: 'Iñigo Montoya',
+      date: '2024/12/22',
+    },
+    {
+      id: 10,
+      name: 'Inspector Clouseau',
+      date: '2024/12/23',
+    },
+    {
+      id: 11,
+      name: 'Norman Bates',
+      date: '2024/12/25',
+    },
+    {
+      id: 12,
+      name: 'Edna Mode',
+      date: '2024/12/31',
+    },
+  ],
+};
--- frontend/src/app/pages/hr/Home.js
+++ frontend/src/app/pages/hr/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hrData } from '../../constants/hrLanding';
 
 const Home = () => {
   return (
@@ -13,6 +14,141 @@ const Home = () => {
             </li>
           </ol>
         </nav>
+      </div>
+      <div className="row ">
+        <div className="col-12 grid-margin">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Talent Acquisition</h4>
+              <div className="row">
+                <div className="table-responsive">
+                  <table className="table text-center" style={{ tableLayout: 'fixed' }}>
+                    <thead>
+                      <tr>
+                        <th>Candidate</th>
+                        <th>Role</th>
+                        <th>Cultural Fit</th>
+                        <th>Tech Assign</th>
+                        <th>Peer Program</th>
+                        <th>Offer Made</th>
+                        <th>Screening OK</th>
+                        <th>Hired</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {hrData.hiring.map(candidate => (
+                        <tr key={candidate.id}>
+                          <td>{candidate.name}</td>
+                          <td>{candidate.role}</td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.culturalFit}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.techAssignment}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.peerReview}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.offerMade}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.screenOk}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-check">
+                              <input
+                                type="checkbox"
+                                defaultChecked={candidate.hiringStatus.hired}
+                                className="form-check-input"
+                              />
+                              <i className="input-helper"></i>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 grid-margin">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">HR Today</h4>
+              <div className="row grid-margin ml-1">
+                Current Ohpeneers:&nbsp;<b>467</b>
+              </div>
+              <div className="row">
+                <div className="col-6 grid-margin">
+                  <h5>
+                    New Hires <span>😊</span>
+                  </h5>
+                  <ul style={{ listStyleType: 'none' }}>
+                    {hrData.newHires.map(person => (
+                      <li key={person.id}>{person.description}</li>
+                    ))}
+                  </ul>
+                  <h5>
+                    Leavers <span>😢</span>
+                  </h5>
+                  <ul style={{ listStyleType: 'none' }}>
+                    {hrData.leavers.map(person => (
+                      <li key={person.id}>{person.description}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="col-6 grid-margin">
+                  <h5>Birthdays this month</h5>
+                  <ul style={{ listStyleType: 'none' }}>
+                    {hrData.upcomingBirthdays.map(person => (
+                      <li key={person.id}>
+                        {person.name}:&nbsp;<b>{person.date}</b>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
