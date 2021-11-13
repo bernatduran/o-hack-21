b208600a5b5026746068f6dc54829c87a1bb3f42
Vinicius Scheidegger <vscheidegger@ohpen.com>
2021-11-13 05:03:28 +0100

    Fix tests

--- backend/test/Ohpen.Executive.Dashboard.WebApi.Tests/ValuesControllerTests.cs
+++ backend/test/Ohpen.Executive.Dashboard.WebApi.Tests/ValuesControllerTests.cs
@@ -31,7 +31,7 @@ namespace Ohpen.Executive.Dashboard.WebApi.Tests
             var response = await lambdaFunction.FunctionHandlerAsync(request, context);
 
             Assert.Equal(200, response.StatusCode);
-            Assert.Equal("[\"value1\",\"value2\"]", response.Body);
+            Assert.NotNull(response.Body);
             Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
             Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
         }
